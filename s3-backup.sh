a88546756a8e71afd5b62c7d707de3b5316de93b
root <root@bda4d464b72a>
2016-05-16 17:59:07 +0000

    Change the copy option to move instead so the step of removing the file wont be needed

--- s3-backup.sh
+++ s3-backup.sh
@@ -4,15 +4,12 @@ COMPRESS="tar -cvzf"
 LOGDIR="/usr/local/apache2/logs"
 BACKUPDIR="/var/backups/logs"
 RMWORK="rm -rf"
-COPY2S3="aws s3 cp"
+COPY2S3="aws s3 mv"
 SRVNAME="apache"
 S3FOLDER="s3://crosslblanco/logs/${SRVNAME}"
 
 echo "Creating and encapsulating logs"
 ${COMPRESS} ${BACKUPDIR}/${SRVNAME}-${DATE}.tar.gz ${LOGDIR}
 
-echo "Copying files to S3 bucket"
+echo "Moving files to S3 bucket"
 ${COPY2S3} ${BACKUPDIR}/* ${S3FOLDER}/${SRVNAME}-${DATE}.tar.gz
-
-echo "Deleting files from backup folder"
-${RMWORK} ${BACKUPDIR}/*
